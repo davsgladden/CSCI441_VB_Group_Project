a5adba696f44de5224b5d95dbc79b6bfde70e543
david.gladden <david.gladden@medfax.com>
2023-03-26 12:59:03 -0400

    Updated DB connection to use Azure DB

--- website/php/functions.php
+++ website/php/functions.php
@@ -3,7 +3,7 @@
 function check_login($con){
     if(isset($_SESSION['user_id'])){
         $id = $_SESSION['user_id'];
-        $query = "SELECT * FROM users WHERE user_id = '$id' LIMIT 1";
+        $query = "SELECT * FROM Users WHERE UserID = '$id' LIMIT 1";
         $result = mysqli_query($con, $query);
         if($result && mysqli_num_rows($result) > 0){
             $user_data = mysqli_fetch_assoc($result);
