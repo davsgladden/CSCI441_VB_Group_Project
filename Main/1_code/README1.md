96ea5fe949f8db70fa04f40a016199d0468e0291
david.gladden <david.gladden@medfax.com>
2023-04-10 16:48:39 -0400

    Updated test printed statements and added README files.

---
+++ Main/1_code/README1.md
@@ -0,0 +1,41 @@
+
+## AgriCom Training
+Web based site that allows for users to be able to mimic the trading of commodities
+in a simulated environment using close to real-time prices.
+
+### Technology
+- PHP 7
+- MySQL
+
+### Testing
+- See README2 in ../2_unit_testing or README3 in ../3_integration_testing
+
+### Project dependencies
+A large majority of our source code is HTML, CSS, Javascript, and PHP. 
+This makes it easily accessible to users who have access to a web browser on a computer.
+
+To run the code you need to have PHP installed and configured on your local machine and
+be connected to the internet for MySQL and API functionalities.
+
+### To Clone & Deploy Application Locally
+- git clone https://github.com/davsgladden/CSCI441_VB_Group_Project.git
+- run 'cd Main/1_code'
+- run 'php -S localhost:8000'
+- Using a web browser, navigate to localhost:8000 or localhost:8000/index.php
+
+All code, including the tests, is inside the Main/1_code directory.
+
+Inside the key folders are:
+- 1_code
+  -  Contains core pages and navigations
+- 1_code/controller/
+  - Contains the core system functions
+- 1_code/entity
+  - Contains entity items that mirror the MySQL schemas
+  - Getter & Setter functions
+  - fetch, insert, update functions where needed
+
+All DB and API config files are in Main/1_code.
+
+All testing files are named ending in *Test.php,
+
--- Main/1_code/orderTest.php
+++ Main/1_code/orderTest.php
@@ -3,18 +3,17 @@
     include("connection.php");
     include("controller/systemController.php");
 
-    $test = 'sell';
-
 /**
  * @param $con
  * @return array
  * @throws Exception
  */
-function createOrderTest($con): array
+function createOrderTest($con, $orderType): array
 {
     $user_data = fetchUser($con, "ID = '2'");
-    $Commodity = fetchCommodity($con, "CommodityID = 5");
-    $Amount = 5;
+    $Commodity = fetchCommodity($con, "CommodityID = 3");
+    $portfolio = fetchPortfolio($con, "UserID = $user_data->ID and CommodityID = $Commodity->CommodityID");
+    $Amount = 1;
     $Price = $Commodity->get_CurrentPrice();
     $Total = $Amount * $Price;
     date_default_timezone_set('America/Chicago'); //Central timezone
@@ -23,20 +22,21 @@ function createOrderTest($con): array
     $Transaction->set_CommodityID($Commodity->get_CommodityID());
     $Transaction->set_Amount($Amount);
     $Transaction->set_Price($Price);
+    $Transaction->set_OrderType($orderType);
     $Transaction->set_TransactionPrice($Total);
     $Transaction->set_TransactionDate(date('Y-m-d H:i:s'));
-    return array($user_data, $Commodity, $Amount, $Price, $Total, $Transaction);
+    return array($user_data, $Commodity, $portfolio, $Amount, $Price, $Total, $Transaction);
 }
 
-    list($user_data, $Commodity, $Amount, $Price, $Total, $Transaction) = createOrderTest($con);
-    if ($test == 'sell') {
-        /** sell order test */
-        $Transaction->set_OrderType('Sell');
-    }
-    if ($test == 'buy') {
-        /** purchase order test */
-        $Transaction->set_OrderType('Buy');
-    }
+    /** sell order test */
+    list($user_data, $Commodity, $portfolio, $Amount, $Price, $Total, $Transaction) = createOrderTest($con, 'Sell');
+
+    echo "Testing SELL functionality for User ID $user_data->ID and Commodity $Commodity->CommodityName.<br>";
+    echo "Current portfolio information for this user and commodity is: <br>";
+    echo json_encode($portfolio);
+    echo "<br>";
+
+    echo "Transaction information is: <br>";
         var_dump($Transaction);
         echo "<br>";
         try {
@@ -48,3 +48,32 @@ function createOrderTest($con): array
         } catch (Exception $e) {
             echo $e->getMessage();
         }
+    $portfolioAfter = fetchPortfolio($con, "UserID = $user_data->ID and CommodityID = $Commodity->CommodityID");
+    echo "Post test portfolio information for this user and commodity is: <br>";
+    echo json_encode($portfolioAfter);
+    echo "<br><br><br>";
+
+    /** Buy order test **/
+    list($user_data, $Commodity, $portfolio, $Amount, $Price, $Total, $Transaction) = createOrderTest($con, 'Buy');
+
+    echo "Testing BUY functionality for User ID $user_data->ID and Commodity $Commodity->CommodityName.<br>";
+    echo "Current portfolio information for this user and commodity is: <br>";
+    echo json_encode($portfolio);
+    echo "<br>";
+
+    echo "Transaction information is: <br>";
+    var_dump($Transaction);
+    echo "<br>";
+    try {
+        if ($Transaction->get_OrderType() == 'Buy') {
+            purchaseOrder($con, $Transaction);
+        } else if ($Transaction->get_OrderType() == 'Sell') {
+            sellOrder($con, $Transaction);
+        }
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    $portfolioAfter = fetchPortfolio($con, "UserID = $user_data->ID and CommodityID = $Commodity->CommodityID");
+    echo "Post test portfolio information for this user and commodity is: <br>";
+    echo json_encode($portfolioAfter);
+    echo "<br>";
