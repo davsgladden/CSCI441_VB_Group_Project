a91c635261753aea5fbb4d16c3042ff13bbdcf14
hashbrownsCal <calvin.chun.ku@gmail.com>
2023-05-07 15:15:30 -0700

    updated apiTest

--- Main/1_code/apiTest.php
+++ Main/1_code/apiTest.php
@@ -3,6 +3,9 @@
     include("connection.php");
     include("controller/systemController.php");
 
+    $endpoint = 'latest';
+    $access_key = '3uiunaq72cjeo2y05yowutzu756r9tuo9h81urdj3fvgm4mpqdhbrc85d8dk';
+    
     //We will be testing WHEAT
     $symbol = 'WHEAT';
     $Commodity = fetchCommodity($con, "Symbol = '$symbol'");
